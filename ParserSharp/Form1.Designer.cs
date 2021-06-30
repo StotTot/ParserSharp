17af2fe75b9ef88973a306c2d4f81c656ddbe02c
Aristotle <a.debride.2016@gmail.com>
2021-06-30 16:18:46 -0400

    Yikes

--- ParserSharp/Form1.Designer.cs
+++ ParserSharp/Form1.Designer.cs
@@ -4,12 +4,12 @@ namespace ParserSharp
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -24,124 +24,133 @@ namespace ParserSharp
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.urlBox = new System.Windows.Forms.TextBox();
-            this.parseButton = new System.Windows.Forms.Button();
-            this.getAllButton = new System.Windows.Forms.Button();
-            this.receiptsList = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.parseButton = new System.Windows.Forms.Button();
+            this.receiptList = new System.Windows.Forms.ListBox();
+            this.refreshButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.receiptImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptImage)).BeginInit();
             this.SuspendLayout();
             // 
             // urlBox
             // 
-            this.urlBox.Location = new System.Drawing.Point(12, 29);
+            this.urlBox.Location = new System.Drawing.Point(12, 33);
             this.urlBox.Name = "urlBox";
-            this.urlBox.PlaceholderText = "url goes here";
-            this.urlBox.Size = new System.Drawing.Size(339, 23);
+            this.urlBox.Size = new System.Drawing.Size(252, 20);
             this.urlBox.TabIndex = 0;
-            this.urlBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // parseButton
-            // 
-            this.parseButton.Location = new System.Drawing.Point(366, 28);
-            this.parseButton.Name = "parseButton";
-            this.parseButton.Size = new System.Drawing.Size(75, 23);
-            this.parseButton.TabIndex = 1;
-            this.parseButton.Text = "Parse";
-            this.parseButton.UseVisualStyleBackColor = true;
-            this.parseButton.Click += new System.EventHandler(this.parseButton_Click);
-            // 
-            // getAllButton
-            // 
-            this.getAllButton.Location = new System.Drawing.Point(366, 58);
-            this.getAllButton.Name = "getAllButton";
-            this.getAllButton.Size = new System.Drawing.Size(75, 23);
-            this.getAllButton.TabIndex = 3;
-            this.getAllButton.Text = "Refresh";
-            this.getAllButton.UseVisualStyleBackColor = true;
-            this.getAllButton.Click += new System.EventHandler(this.getAllButton_Click);
-            // 
-            // receiptsList
-            // 
-            this.receiptsList.ColumnWidth = 1;
-            this.receiptsList.FormattingEnabled = true;
-            this.receiptsList.ItemHeight = 15;
-            this.receiptsList.Location = new System.Drawing.Point(12, 101);
-            this.receiptsList.MultiColumn = true;
-            this.receiptsList.Name = "receiptsList";
-            this.receiptsList.Size = new System.Drawing.Size(429, 334);
-            this.receiptsList.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Location = new System.Drawing.Point(13, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 15);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "URL:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // parseButton
+            // 
+            this.parseButton.Location = new System.Drawing.Point(302, 30);
+            this.parseButton.Name = "parseButton";
+            this.parseButton.Size = new System.Drawing.Size(116, 23);
+            this.parseButton.TabIndex = 2;
+            this.parseButton.Text = "Parse";
+            this.parseButton.UseVisualStyleBackColor = true;
+            this.parseButton.Click += new System.EventHandler(this.parseButton_Click);
+            // 
+            // receiptList
+            // 
+            this.receiptList.FormattingEnabled = true;
+            this.receiptList.Location = new System.Drawing.Point(12, 92);
+            this.receiptList.Name = "receiptList";
+            this.receiptList.Size = new System.Drawing.Size(200, 342);
+            this.receiptList.TabIndex = 3;
+            this.receiptList.SelectedIndexChanged += new System.EventHandler(this.receiptList_SelectedIndexChangedAsync);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(302, 60);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(116, 23);
+            this.refreshButton.TabIndex = 4;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 80);
+            this.label2.Location = new System.Drawing.Point(12, 69);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(18, 15);
-            this.label2.TabIndex = 6;
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 5;
             this.label2.Text = "ID";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(37, 80);
+            this.label3.Location = new System.Drawing.Point(36, 70);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(39, 15);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "TOTAL";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Total";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(82, 80);
+            this.label4.Location = new System.Drawing.Point(83, 70);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 15);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "TAXES ";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Taxes";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(130, 80);
+            this.label5.Location = new System.Drawing.Point(137, 70);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(34, 15);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "DATE";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Date";
+            // 
+            // receiptImage
+            // 
+            this.receiptImage.Location = new System.Drawing.Point(231, 92);
+            this.receiptImage.Name = "receiptImage";
+            this.receiptImage.Size = new System.Drawing.Size(187, 342);
+            this.receiptImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.receiptImage.TabIndex = 9;
+            this.receiptImage.TabStop = false;
+            this.receiptImage.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(457, 450);
+            this.ClientSize = new System.Drawing.Size(429, 450);
+            this.Controls.Add(this.receiptImage);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.receiptsList);
-            this.Controls.Add(this.getAllButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.receiptList);
             this.Controls.Add(this.parseButton);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.urlBox);
             this.Name = "Form1";
-            this.Text = "ParserSharp";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.receiptImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,14 +159,15 @@ namespace ParserSharp
         #endregion
 
         private System.Windows.Forms.TextBox urlBox;
-        private System.Windows.Forms.Button parseButton;
-        private System.Windows.Forms.Button getAllButton;
-        private System.Windows.Forms.ListBox receiptsList;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button parseButton;
+        private System.Windows.Forms.ListBox receiptList;
+        private System.Windows.Forms.Button refreshButton;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox receiptImage;
     }
 }
 
--- ParserSharp/Form1.cs
+++ ParserSharp/Form1.cs
@@ -1,15 +1,13 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Net.Http.Headers;
 
 namespace ParserSharp
 {
@@ -20,16 +18,12 @@ namespace ParserSharp
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private async void parseButton_Click(object sender, EventArgs e)
         {
             HttpClient client = new HttpClient();
-            try {
-                StringContent httpContent = new StringContent(urlBox.Text, Encoding.UTF8, "plain/text") ;
+            try
+            {
+                StringContent httpContent = new StringContent(urlBox.Text, Encoding.UTF8, "plain/text");
                 HttpResponseMessage response = await client.PostAsync("http://localhost:8080/parse", httpContent);
                 response.EnsureSuccessStatusCode();
                 Console.WriteLine(response);
@@ -41,26 +35,26 @@ namespace ParserSharp
             }
         }
 
-        private async void getAllButton_Click(object sender, EventArgs e)
+        private async void refreshButton_Click(object sender, EventArgs e)
         {
             HttpClient client = new HttpClient();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             try
             {
-                receiptsList.Items.Clear();
+                receiptList.Items.Clear();
 
                 HttpResponseMessage response = await client.GetAsync("http://localhost:8080/receipts");
                 response.EnsureSuccessStatusCode();
                 //receiptsList.Items.Add(response.Content.ReadAsStringAsync().Result);
                 Console.WriteLine(response);
                 var receipts = response.Content.ReadAsStringAsync().Result;
-                List<receipt> rList = JsonConvert.DeserializeObject<List<receipt>>(receipts);
+                List<Receipt> rList = JsonConvert.DeserializeObject<List<Receipt>>(receipts);
                 //var receipts = JsonConvert.DeserializeObject<List<receipt>>(response.Content.ReadAsStringAsync().);
-                
-                rList.ForEach(delegate (receipt r)
+
+                rList.ForEach(delegate (Receipt r)
                 {
                     DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds(r.date);
-                    receiptsList.Items.Add(r.id + "      $" + r.total + "      $" + r.taxesPaid + "      " + dateTimeOffset.Date.ToString("MM/dd/yy"));
+                    receiptList.Items.Add(r.id + "      $" + r.total + "      $" + r.taxesPaid + "      " + dateTimeOffset.Date.ToString("MM/dd/yy"));
                 });
 
             }
@@ -71,25 +65,46 @@ namespace ParserSharp
             }
         }
 
-        private void receiptsList_SelectedIndexChanged(object sender, EventArgs e)
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private async void receiptList_SelectedIndexChangedAsync(object sender, EventArgs e)
         {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            String r = receiptList.SelectedItem.ToString();
+            List<String> result = r.Split(' ').ToList();
+            String id = result[0];
+            Receipt receipt;
+            try 
+            {
+                HttpResponseMessage response = await client.GetAsync("http://localhost:8080/receipts/" + id);
+                response.EnsureSuccessStatusCode();
+                var content = response.Content.ReadAsStringAsync().Result;
+                receipt = JsonConvert.DeserializeObject<Receipt>(content);
+                receiptImage.Load(receipt.url);
+            
+            }
+            catch (HttpRequestException err)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", err.Message);
+            }
 
         }
     }
 
-    public class receipt {
+    public class Receipt
+    {
 
         public int id;
         public double total;
         public double taxesPaid;
         public long date;
-         
-    
-    
+        public string url;
+
+
     }
 }
