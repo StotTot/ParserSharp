430318188db82bfca8775e6416f87d02a08bcdae
Aristotle <a.debride.2016@gmail.com>
2021-07-02 11:49:38 -0400

    Acutally working, very pog

--- ParserSharp/Form1.Designer.cs
+++ ParserSharp/Form1.Designer.cs
@@ -29,24 +29,31 @@ namespace ParserSharp
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.urlBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.parseButton = new System.Windows.Forms.Button();
-            this.receiptList = new System.Windows.Forms.ListBox();
             this.refreshButton = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.receiptImage = new System.Windows.Forms.PictureBox();
+            this.receiptGrid = new System.Windows.Forms.DataGridView();
+            this.receiptBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.receiptBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ID = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.TOTAL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TAXES = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DATE = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.receiptImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptBindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // urlBox
             // 
             this.urlBox.Location = new System.Drawing.Point(12, 33);
             this.urlBox.Name = "urlBox";
-            this.urlBox.Size = new System.Drawing.Size(252, 20);
+            this.urlBox.Size = new System.Drawing.Size(264, 20);
             this.urlBox.TabIndex = 0;
             // 
             // label1
@@ -68,15 +75,6 @@ namespace ParserSharp
             this.parseButton.UseVisualStyleBackColor = true;
             this.parseButton.Click += new System.EventHandler(this.parseButton_Click);
             // 
-            // receiptList
-            // 
-            this.receiptList.FormattingEnabled = true;
-            this.receiptList.Location = new System.Drawing.Point(12, 92);
-            this.receiptList.Name = "receiptList";
-            this.receiptList.Size = new System.Drawing.Size(200, 342);
-            this.receiptList.TabIndex = 3;
-            this.receiptList.SelectedIndexChanged += new System.EventHandler(this.receiptList_SelectedIndexChangedAsync);
-            // 
             // refreshButton
             // 
             this.refreshButton.Location = new System.Drawing.Point(302, 60);
@@ -87,70 +85,88 @@ namespace ParserSharp
             this.refreshButton.UseVisualStyleBackColor = true;
             this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 69);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(18, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "ID";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(36, 70);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Total";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(83, 70);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(36, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Taxes";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(137, 70);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(30, 13);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Date";
-            // 
             // receiptImage
             // 
-            this.receiptImage.Location = new System.Drawing.Point(231, 92);
+            this.receiptImage.Location = new System.Drawing.Point(302, 92);
             this.receiptImage.Name = "receiptImage";
-            this.receiptImage.Size = new System.Drawing.Size(187, 342);
+            this.receiptImage.Size = new System.Drawing.Size(224, 342);
             this.receiptImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.receiptImage.TabIndex = 9;
             this.receiptImage.TabStop = false;
             this.receiptImage.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // receiptGrid
+            // 
+            this.receiptGrid.AllowUserToAddRows = false;
+            this.receiptGrid.AllowUserToDeleteRows = false;
+            this.receiptGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.receiptGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.TOTAL,
+            this.TAXES,
+            this.DATE});
+            this.receiptGrid.Location = new System.Drawing.Point(12, 92);
+            this.receiptGrid.Name = "receiptGrid";
+            this.receiptGrid.ReadOnly = true;
+            this.receiptGrid.Size = new System.Drawing.Size(263, 342);
+            this.receiptGrid.TabIndex = 10;
+            this.receiptGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.receiptGrid_CellContentClick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 59);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(263, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ID.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.ID.Width = 30;
+            // 
+            // TOTAL
+            // 
+            this.TOTAL.HeaderText = "TOTAL";
+            this.TOTAL.Name = "TOTAL";
+            this.TOTAL.ReadOnly = true;
+            this.TOTAL.Width = 60;
+            // 
+            // TAXES
+            // 
+            this.TAXES.HeaderText = "TAXES";
+            this.TAXES.Name = "TAXES";
+            this.TAXES.ReadOnly = true;
+            this.TAXES.Width = 60;
+            // 
+            // DATE
+            // 
+            this.DATE.HeaderText = "DATE";
+            this.DATE.Name = "DATE";
+            this.DATE.ReadOnly = true;
+            this.DATE.Width = 70;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(429, 450);
+            this.ClientSize = new System.Drawing.Size(541, 455);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.receiptGrid);
             this.Controls.Add(this.receiptImage);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.refreshButton);
-            this.Controls.Add(this.receiptList);
             this.Controls.Add(this.parseButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.urlBox);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Parser Sharp";
             ((System.ComponentModel.ISupportInitialize)(this.receiptImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptBindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,13 +177,16 @@ namespace ParserSharp
         private System.Windows.Forms.TextBox urlBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button parseButton;
-        private System.Windows.Forms.ListBox receiptList;
         private System.Windows.Forms.Button refreshButton;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.PictureBox receiptImage;
+        private System.Windows.Forms.DataGridView receiptGrid;
+        private System.Windows.Forms.BindingSource receiptBindingSource;
+        private System.Windows.Forms.BindingSource receiptBindingSource1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DataGridViewButtonColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TOTAL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TAXES;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DATE;
     }
 }
 
